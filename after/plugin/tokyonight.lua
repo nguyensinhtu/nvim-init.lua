b852d16bb9d145d4c611bddbb591aaad8a94cfe8
Tu Nguyen SInh <tunguyensinh@Tus-MacBook-Pro.local>
2023-10-08 12:47:57 +0700

    chore: check theme existing

--- after/plugin/tokyonight.lua
+++ after/plugin/tokyonight.lua
@@ -1,4 +1,9 @@
-require("tokyonight").setup({
+local status_ok, theme = pcall(require, "tokyonight")
+if not status_ok then
+	return
+end
+
+theme.setup({
   -- your configuration comes here
   -- or leave it empty to use the default settings
   style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
