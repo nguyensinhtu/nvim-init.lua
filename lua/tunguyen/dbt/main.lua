da9ffdbc8c3d25cb7fc00dc917dfecbcfdeb18c2
Tu Nguyen SInh <tunguyensinh@Tus-MacBook-Pro.local>
2024-04-24 16:07:13 +0700

    write to buffer

--- lua/tunguyen/dbt/main.lua
+++ lua/tunguyen/dbt/main.lua
@@ -8,166 +8,170 @@ local display = require("dbtpal.display")
 local M = {}
 
 M._cmd_run_operation = function(operation, args, arg_spec)
-	return M._create_job({ "run-operation", operation }, args, arg_spec)
+    return M._create_job({ "run-operation", operation }, args, arg_spec, vim.api.nvim_buf_get_name(0))
 end
 
 M.generate_model_yaml = function(args)
-	local arg_spec = {
-		{
-			name = "model_names",
-			required = true,
-			type = "list",
-		},
-		{
-			name = "include_data_types",
-			required = false,
-			default = false,
-			type = "boolean",
-		},
-	}
-	return M._cmd_run_operation("generate_model_yaml", args, arg_spec)
+    local arg_spec = {
+        {
+            name = "model_names",
+            required = true,
+            type = "list",
+        },
+        {
+            name = "include_data_types",
+            required = false,
+            default = false,
+            type = "boolean",
+        },
+    }
+    return M._cmd_run_operation("generate_model_yaml", args, arg_spec)
 end
 
 M.generate_base_model = function(args)
-	local arg_spec = {
-		{
-			name = "source_name",
-			required = true,
-			type = "string",
-		},
-		{
-			name = "table_name",
-			required = true,
-			type = "string",
-		},
-	}
-	return M._cmd_run_operation("generate_base_model", args, arg_spec)
+    local arg_spec = {
+        {
+            name = "source_name",
+            required = true,
+            type = "string",
+        },
+        {
+            name = "table_name",
+            required = true,
+            type = "string",
+        },
+    }
+    return M._cmd_run_operation("generate_base_model", args, arg_spec)
 end
 
 M.generate_source = function(args)
-	local arg_spec = {
-		{
-			name = "schema_name",
-			required = true,
-			type = "string",
-		},
+    local arg_spec = {
+        {
+            name = "schema_name",
+            required = true,
+            type = "string",
+        },
 
-		{
-			name = "database_name",
-			required = false,
-			type = "string",
-		},
-		{
-			name = "table_names",
-			required = false,
-			type = "list",
-		},
+        {
+            name = "database_name",
+            required = false,
+            type = "string",
+        },
+        {
+            name = "table_names",
+            required = false,
+            type = "list",
+        },
 
-		{
-			name = "generate_columns",
-			required = false,
-			type = "boolean",
-			default = "false",
-		},
+        {
+            name = "generate_columns",
+            required = false,
+            type = "boolean",
+            default = "false",
+        },
 
-		{
-			name = "include_descriptions",
-			required = false,
-			type = "boolean",
-			default = "false",
-		},
-		{
-			name = "include_data_types",
-			required = false,
-			type = "boolean",
-			default = "true",
-		},
+        {
+            name = "include_descriptions",
+            required = false,
+            type = "boolean",
+            default = "false",
+        },
+        {
+            name = "include_data_types",
+            required = false,
+            type = "boolean",
+            default = "true",
+        },
 
-		{
-			name = "include_database",
-			required = false,
-			type = "boolean",
-			default = "false",
-		},
-		{
-			name = "include_schema",
-			required = false,
-			type = "boolean",
-			default = "false",
-		},
-	}
-	return M._cmd_run_operation("generate_source", args, arg_spec)
+        {
+            name = "include_database",
+            required = false,
+            type = "boolean",
+            default = "false",
+        },
+        {
+            name = "include_schema",
+            required = false,
+            type = "boolean",
+            default = "false",
+        },
+    }
+    return M._cmd_run_operation("generate_source", args, arg_spec)
 end
 
-M._create_job = function(cmd, args, arg_spec)
-	if client.get_dbt_project_root() == nil then
-		log.warn(
-			"Could not detect dbt project dir, try setting it manually or make sure this file is in a dbt project folder"
-		)
-		return
-	end
+M._create_job = function(cmd, args, arg_spec, bufname)
+    if client.get_dbt_project_root() == nil then
+        log.warn(
+            "Could not detect dbt project dir, try setting it manually or make sure this file is in a dbt project folder"
+        )
+        return
+    end
 
-	local onexit_success = function(data)
-		local lnum = vfn.getcurpos()[2]
-		table.insert(data, 1, "")
-		vfn.append(lnum, data)
-		vim.cmd("w")
-	end
+    local onexit_success = function(data, bufname)
+        -- Get bufnr from bufname
+        local bufnr = vim.uri_to_bufnr("file:///" .. bufname)
 
-	local onexit_failed = function(data)
-		display.popup(data)
-	end
+        if not vim.api.nvim_buf_is_loaded(bufnr) then
+            return
+        end
 
-	if args == "" then
-		args = nil
-	end
+        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, data)
+    end
 
-	local config = {
-		path_to_dbt = "dbt",
-		path_to_dbt_project = client.get_dbt_project_root(),
-		path_to_dbt_profiles_dir = client.get_dbt_project_root(),
-	}
+    local onexit_failed = function(data)
+        display.popup(data)
+    end
 
-	local dbt_path, cmd_args, err = Utils.build_generator_path_args(cmd, args, config, arg_spec)
+    if args == "" then
+        args = nil
+    end
 
-	-- log.debug("Executing dbt command: ", dbt_path, cmd_args)
+    local config = {
+        path_to_dbt = "dbt",
+        path_to_dbt_project = client.get_dbt_project_root(),
+        path_to_dbt_profiles_dir = client.get_dbt_project_root(),
+    }
 
-	if err then
-		log.error("Failed to build dbt command: ", err)
-		return
-	end
+    local dbt_path, cmd_args, err = Utils.build_generator_path_args(cmd, args, config, arg_spec)
 
-	local response = {}
-	local job = J:new({
-		command = dbt_path,
-		args = cmd_args,
-		on_exit = function(j, code)
-			if code == 1 then
-				vim.list_extend(response, j:result())
-				log.info("dbt command encounted a handled error, see popup for details")
-			elseif code >= 2 then
-				table.insert(response, "Failed to run dbt command. Exit Code: " .. code .. "\n")
-				local a = table.concat(cmd_args, " ") or ""
-				local err = string.format("dbt command failed: dbt_path: %s args: %s\n\n", dbt_path, a)
-				table.insert(response, "------------\n")
-				table.insert(response, err)
-				vim.list_extend(response, j:result())
-				vim.list_extend(response, j:stderr_result())
-			else
-				vim.schedule(function()
-					onexit_success(j:result())
-				end)
-			end
+    -- log.debug("Executing dbt command: ", dbt_path, cmd_args)
 
-			if code >= 1 then
-				vim.schedule(function()
-					onexit_failed(response)
-				end)
-			end
-		end,
-	})
-	job:start()
-	return job
+    if err then
+        log.error("Failed to build dbt command: ", err)
+        return
+    end
+
+    local response = {}
+    local job = J:new({
+        command = dbt_path,
+        args = cmd_args,
+        on_exit = function(j, code)
+            if code == 1 then
+                vim.list_extend(response, j:result())
+                log.info("dbt command encounted a handled error, see popup for details")
+            elseif code >= 2 then
+                table.insert(response, "Failed to run dbt command. Exit Code: " .. code .. "\n")
+                local a = table.concat(cmd_args, " ") or ""
+                local err = string.format("dbt command failed: dbt_path: %s args: %s\n\n", dbt_path, a)
+                table.insert(response, "------------\n")
+                table.insert(response, err)
+                vim.list_extend(response, j:result())
+                vim.list_extend(response, j:stderr_result())
+            else
+                vim.schedule(function()
+                    onexit_success(j:result(), bufname)
+                end)
+            end
+
+            if code >= 1 then
+                vim.schedule(function()
+                    onexit_failed(response)
+                end)
+            end
+        end,
+    })
+    job:start()
+    return job
 end
 
 return M
