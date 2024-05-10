1c87cd89fcc238a0d13a7384b4b2aff7953eae5c
Tu Nguyen SInh <tunguyensinh@Tus-MacBook-Pro.local>
2024-05-10 10:20:58 +0700

    add oil, replacement for nvim-tree

--- lua/tunguyen/plugins/oil.lua
+++ lua/tunguyen/plugins/oil.lua
@@ -1,3 +1,13 @@
 require("oil").setup({})
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+vim.api.nvim_create_autocmd("DirChanged", {
+    pattern = "*",
+    callback = function()
+        vim.notify("DirChanged")
+        vim.schedule_wrap(require("oil").open)(vim.v.event.cwd)
+    end,
+})
+
+-- vim.keymap.set("n", "<C-p>", "<cmd>lua actions.preview<cr>", { desc = "Open parent directory" })
